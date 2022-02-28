78726c03c0d71b1863416e45786aa992ecb532f2
Stefan <7stefanadrian@gmail.com>
2022-02-28 17:00:48 +0200

    empty

--- Exercices/main_class_NumbersList.cpp
+++ Exercices/main_class_NumbersList.cpp
@@ -2,6 +2,16 @@
 #include "NumberList.h"
 
 using namespace std; 
-
+int main()
+{
+    NumberList list1;
+    // list1.Init();//initialise the count attribute with 0
+    list1.Add(12);
+    list1.Add(23);
+    list1.Add(34);
+    list1.Add(41);
+    list1.Sort();
+    list1.Print();
+}
 
 
