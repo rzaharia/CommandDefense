fed39508e5e5bcc33b3c9201b7d2d3b100e95491
Raul Zaharia <zahariarauul@gmail.com>
2022-03-02 13:09:36 +0200

    added labwork

---
+++ week2/LabWork/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdio.h>
+
+int some_function(float y)
+{
+    return 0;
+}
+
+struct data
+{
+    int x=2, y=3, z=4;
+};
+
+int main()
+{
+    //char* buffer; -> 4 int
+    //malloc vs calloc
+    //free
+    
+    //int* x = new int;
+    //set x value to 5
+
+    //int[] -> delete[]
+    //new vs new[], delete vs delete[]
+    //exemplu clase
+
+    /*int x = 5;
+    int* y = &x;
+    *y = 10;*/
+
+    //data d;
+    //int* p = (int*) & d.x;
+    //p++;
+    //*p = 10;
+
+    //int* v = new int[10];
+    //for (int i = 0; i < 10; i++)
+    //{
+    //    v[i] = 10;
+    //    //*(v + i) = 10;
+    //}
+    //delete[] v;
+
+    //exemplu container nume 
+    //alocat dinamic
+
+    //*x = some_function;
+    //apel x
+
+    //class with name as param
+    //deletes name after class and call function that uses it
+
+    return 0;
+}
